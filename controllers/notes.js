f3e65b040a43f7e694a4c5194eee157ac2cf1160
Ragnar Pálsson <raggipals@gmail.com>
2023-09-07 20:20:27 +0000

    Finished section 4

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async(request, response) => {
     const notes = await Note.find({})
@@ -17,10 +18,24 @@ notesRouter.get('/:id', async (request, response, next) => {
     }
 })
 
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
     const users = await User.find({})
-    const user = users.find((u) => u.id === body.userId)
+    const user = users.find((u) => u.id === decodedToken.id)
 
     const note = new Note({
         content: body.content,
